e815307409371db465bd8e5176fb9ae3206250cb
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2023-05-14 00:29:11 +0530

    Added get all airplanes api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -1,59 +1,39 @@
-class CrudRepository{
-    constructor(model){
+class CrudRepository {
+    constructor(model) {
         this.model = model;
     }
 
-    async create(data){
-            const response = await this.model.create(data);
-            return response;
+    async create(data) {
+        const response = await this.model.create(data);
+        return response;
     }
 
-    async destroy(data){
-        try{
-            const response = await this.model.destroy({
-                where: {
-                    id: data
-                }
-            });
-            return response;
-        } catch(error){
-            Logger.error('Something went wrong in the Crud Repo: delete');
-            throw error;
-        }
+    async destroy(data) {
+        const response = await this.model.destroy({
+            where: {
+                id: data
+            }
+        });
+        return response;
     }
 
-    async get(data){
-        try{
-            const response = await this.model.findByPk(data)
-            return response;
-        } catch(error){
-            Logger.error('Something went wrong in the Crud Repo: get');
-            throw error;
-        }
+    async get(data) {
+        const response = await this.model.findByPk(data)
+        return response;
     }
 
-    async getAll(data){
-        try{
-            const response = await this.model.findAll(data)
-            return response;
-        } catch(error){
-            Logger.error('Something went wrong in the Crud Repo: get');
-            throw error;
-        }
+    async getAll(data) {
+        const response = await this.model.findAll(data)
+        return response;
     }
 
-    async update(id, data){
-        try{
-            const response = await this.model.update(data, {
-                where:{
-                    id: id
-                }
-            })
-            return response;
-        } catch(error){
-            Logger.error('Something went wrong in the Crud Repo: get');
-            throw error;
-        }
+    async update(id, data) {
+        const response = await this.model.update(data, {
+            where: {
+                id: id
+            }
+        })
+        return response;
     }
 }
 
