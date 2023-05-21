1e240f1524d92a1dfd1000caa71827b0080f7169
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2023-05-21 21:38:34 +0530

    Added new migration to add foreign key constraint in one to many relation ```  npx sequelize migration:generate --name update-city-airport-association
    ```

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ const apiRoutes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT,  async() => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    
+
     //bad code
-    const {City , Airport} = require('./models');
+    const { City, Airport } = require('./models');
     const bengaluru = await City.findByPk(2);
     console.log(bengaluru);
     // const airport = await Airport.create({name: 'Kempegowda Airport', code: 'BLR'});
@@ -24,9 +24,18 @@ app.listen(ServerConfig.PORT,  async() => {
     // const kmpairport = await Airport.findByPk(2);
     // bengaluru.removeAirport(kmpairport);
 
+    // await City.destroy({
+    //     where:{
+    //         id: 8,
+    //     }
+    // })
+
+    // const city = await City.findByPk(3);
+    // await city.createAirport({ name: 'Kolkata airport', code: 'CCU' });
+
     await City.destroy({
         where:{
-            id: 8,
+            id:3,
         }
     })
 });
