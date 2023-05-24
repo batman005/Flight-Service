22304bd55d956f1bd2f93af4022b11f8e67477ac
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2023-05-25 02:13:46 +0530

    Added complex join for fetching all airports with city

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize');
+
 const CrudRepository = require('./crud-repository');
-const { Flight, Airplane, Airport } = require('../models');
+const { Flight, Airplane, Airport, City } = require('../models');
 
 
 class FlightRepository extends CrudRepository {
     constructor() {
         super(Flight);
     }
+
     async getAllFlights(filter, sort) {
         const response = await Flight.findAll({
             where: filter,
@@ -15,28 +17,36 @@ class FlightRepository extends CrudRepository {
                 {
                     model: Airplane,
                     required: true,
-                    as: 'airplaneDetail'
+                    as: 'airplaneDetail',
                 },
                 {
                     model: Airport,
                     required: true,
                     as: 'departureAirport',
-                    on: {
-                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code")),
+                    on : {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
                     },
+                    include: {
+                        model: City,
+                        required: true
+                    }
                 },
                 {
                     model: Airport,
                     required: true,
                     as: 'arrivalAirport',
-                    on: {
+                    on : {
                         col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
                     },
-                }]
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                }
+            ]
         });
         return response;
     }
-
 }
 
 module.exports = FlightRepository;
